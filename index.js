b92ff2e18daeb243758a3be24eecbad7fcafde54
Elijah Noble <elijahnobledev@gmail.com>
2022-03-31 21:04:09 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(arr,target){
+    const result = arr.filter(e => e.toLowerCase() == target.toLowerCase())
+    return result
+}
+
+function fuzzyMatch(arr,target){
+    const result = arr.filter(e => e.slice(0,target.length).toLowerCase() == target.toLowerCase())
+    return result
+}
+
+function matchName(arr,target){
+    const result = arr.filter(e => e.name.toLowerCase() == target.toLowerCase())
+    return result 
+}
